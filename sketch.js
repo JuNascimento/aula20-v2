31df48e763aa5f7de441fb2e5b151d54c2eaaa53
Julia de Araújo Nascimento <ju.araujo11@yahoo.com.br>
2022-04-04 19:01:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+const Body = Matter.Body
+
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background('black')
+}
